48c50a01759a309b72dde88b0a15fc7d3991d9bb
Eric Hamilton <inarchus@gmail.com>
2024-05-26 17:03:42 -0400

    I will use this for the main shell and other operations

---
+++ kernel.c
@@ -0,0 +1,30 @@
+/*
+extern void hexdump(unsigned int memory_location);
+
+void hexdump(unsigned int memory_location)
+{
+	
+}
+*/
+
+extern int strings_equal(char * string1, char * string2);
+extern void cgetline(char * in_string, int position);
+extern void cprintline(char * out_string, int position);
+
+void main_shell(char * in_string)
+{
+	int position = 0;
+	while(1)
+	{
+		// blank the line out
+		cprintline(" ", 0x1700);
+		cgetline(in_string, 0x1700);
+		cprintline(in_string, position);
+		position += 0x01 << 8;
+		if (position >> 8 == 0x17)
+		{
+			position = 0;
+		}
+	}
+}
+

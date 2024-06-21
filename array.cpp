f73682a031f9b0376839329433dccf00dee2082a
Eric Hamilton <inarchus@gmail.com>
2024-06-21 02:55:15 -0400

    first implementation of array using templates

---
+++ array.cpp
@@ -0,0 +1,110 @@
+#include "array.h"
+#include "memory.h"
+/*
+		t * p_array;
+		unsigned int n_size;		// size of the Array object, number of elements inserted
+		unsigned int n_capacity;	// total capacity of the actual p_array, rather than just the number currently filled.
+*/
+template<typename t>
+Array<t>::Array(unsigned int default_size)
+	: n_capacity(default_size), n_size(0)
+{
+	p_array = new t[n_capacity];
+}
+
+template<typename t>
+Array<t>::Array(const Array<t> & rhs)
+{
+	
+}
+
+template<typename t>
+Array<t> & Array<t>::operator = (const Array<t> & rhs)
+{
+	return *this;
+}
+
+template<typename t>
+bool Array<t>::operator == (const Array<t> & rhs) const
+{
+	return false;
+}
+
+template<typename t>
+void Array<t>::append(const t & new_item)
+{
+	if (n_size < n_capacity)
+	{
+		p_array[n_size++] = new_item;
+	}
+	else
+	{
+		t * new_array = new t[2 * n_capacity];
+		for(unsigned int i = 0; i < n_size; i++)
+		{
+			new_array[i] = p_array[i];
+		}
+		n_size++;
+		n_capacity *= 2;
+		delete p_array;
+		p_array = new_array;
+	}
+}
+
+template<typename t>
+t & Array<t>::operator [] (unsigned int index)
+{
+	if(index < n_size)
+		return p_array[index];
+	else
+		return error_value;
+}
+
+template<typename t>
+void Array<t>::set_error_value(const t & err)
+{
+	error_value = err;
+}
+
+template<typename t>
+const t & Array<t>::get_error_value() const
+{
+	return error_value;
+}
+
+
+template<typename t>
+inline unsigned int Array<t>::size() const
+{
+	return n_size;
+}
+
+template<typename t>
+inline unsigned int Array<t>::capacity() const
+{
+	return n_capacity;
+}
+
+template<typename t>
+bool Array<t>::exists(const t & item)
+{
+	return false;
+}
+
+template<typename t>
+unsigned int Array<t>::find_index(const t & item)
+{
+	return 0;
+}
+
+template<typename t>
+bool Array<t>::remove_element(const t & item)
+{
+	return false;
+}
+
+template<typename t>
+Array<t>::~Array()
+{
+	delete [] p_array;
+}
---
+++ array.h
@@ -0,0 +1,36 @@
+#ifndef __ARRAY_H__
+#define __ARRAY_H__
+
+template<typename t>
+class Array
+{
+	public:
+		Array(unsigned int default_size = 10);
+		Array(const Array<t> & rhs);
+		
+		Array<t> & operator = (const Array<t> & rhs);
+		bool operator == (const Array<t> & rhs) const;
+		void append(const t & new_item);
+		t & operator [] (unsigned int index);
+		
+		inline unsigned int size() const;
+		inline unsigned int capacity() const;
+		
+		bool exists(const t & item);
+		unsigned int find_index(const t & item);
+		bool remove_element(const t & item);
+		
+		void set_error_value(const t & err);
+		const t & get_error_value() const;
+		
+		~Array();
+	private:
+		t * p_array;
+		unsigned int n_size;		// size of the Array object, number of elements inserted
+		unsigned int n_capacity;	// total capacity of the actual p_array, rather than just the number currently filled.
+		t error_value;
+};
+
+#include "array.cpp"
+
+#endif

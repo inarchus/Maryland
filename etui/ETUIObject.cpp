05ab353432b24185887a1050da622cdd7cd08900
Eric Hamilton <inarchus@gmail.com>
2024-06-29 18:50:43 -0400

    in development, but nearly functional

--- etui/ETUIObject.cpp
+++ etui/ETUIObject.cpp
@@ -1,84 +1,45 @@
 #include "ETUIObject.h"
 
+extern "C" void __fastcall print_hex_dword(unsigned int dword, unsigned int position);
+
+int ETUIObject::screen_width = 80;
 
 ETUIObject::ETUIObject()
-	: row_position(0), col_position(0), row_size(10), col_size(10), visible(false), p_parent(nullptr), base_pointer(this)
+	: row_position(0), col_position(0), row_size(10), col_size(10), visible(false), p_parent(nullptr), base_pointer(this), border_symbol(0xb1)
 {
-
-}
-
-inline bool ETUIObject::getVisible() const
-{
-	return visible;
-}
-
-inline unsigned int ETUIObject::getRPosition() const
-{
-	return row_position;
-}
-
-inline unsigned int ETUIObject::getCPosition() const
-{
-	return col_position;
-}
-
-inline unsigned int ETUIObject::getRSize() const
-{
-	return row_size;
-}
-
-inline unsigned int ETUIObject::getCSize() const
-{
-	return col_size;
+	
 }
 
 void ETUIObject::redraw()
 {
-	// blank stub because this object is a default basic object.  Could make it abstract if we wanted to...
-	// base_pointer->redraw();
+
 }
 
-inline bool ETUIObject::setVisible(bool new_visible)
-{
-	visible = new_visible;
-	redraw();
-	return visible;
-}
-
-inline unsigned int ETUIObject::setRPosition(unsigned int new_row)
-{
-	row_position = new_row;
-	redraw();
-	return row_position;
-}
-
-inline unsigned int ETUIObject::setCPosition(unsigned int new_col)
-{
-	col_position = new_col;
-	redraw();
-	return col_position;
-}
-
-inline unsigned int ETUIObject::setRSize(unsigned int new_row_size)
-{
-	row_size = new_row_size;
-	redraw();
-	return row_size;
-}
-
-inline unsigned int ETUIObject::setCSize(unsigned int new_col_size)
-{
-	col_size = new_col_size;
-	redraw();
-	return col_size;
-}
-
-inline ETUIObject * ETUIObject::GetParent() const
+ETUIObject * ETUIObject::GetParent() const
 {
 	return p_parent;
 }
 
-inline void ETUIObject::SetParent(ETUIObject * p_new_parent)
+void ETUIObject::SetParent(ETUIObject * p_new_parent)
 {
 	p_parent = p_new_parent;
 }
+
+void ETUIObject::printToBuffer(int x_pos, int y_pos, char out_char, byte format)
+{
+	word * p_screen = (word*)0xb8000;
+	p_screen[x_pos * screen_width + y_pos] = (format << 8) | out_char;	
+	print_hex_dword((dword)&screen_width, 0x0500);
+}
+
+/*
+asm (
+		"movl %0, %%eax;"
+		"movxzb 80, %%ebx;"
+		"movb %2, %%al;"
+		"movl %3, %%ecx;"
+		: 
+		: "r" (x_pos), "r" (y_pos), "r" (out_char), "r" (format), "r"(screen_width)
+		: 
+	);	
+	*/
--- etui/ETUIObject.h
+++ etui/ETUIObject.h
@@ -1,6 +1,8 @@
 #ifndef __ETUI_OBJECT__
 #define __ETUI_OBJECT__
 
+#include "../memory.h"
+
 enum EBorderType {borderless, single_line, double_line};
 
 class ETUIObject
@@ -11,15 +13,23 @@ class ETUIObject
 		inline unsigned int getRPosition() const;
 		inline unsigned int getCPosition() const;
 
-		inline unsigned int getRSize() const;
-		inline unsigned int getCSize() const;
+		inline unsigned int getRSize() const { return row_size; }
+		inline unsigned int getCSize() const { return col_size; }
 		
-		inline bool setVisible(bool new_visible);
-		inline unsigned int setRPosition(unsigned int new_row);
-		inline unsigned int setCPosition(unsigned int new_col);
+		inline bool setVisible(bool new_visible) 			   { visible = new_visible; redraw(); return visible; }
+		inline unsigned int setRPosition(unsigned int new_row) { row_position = new_row; redraw(); return row_position; }
+		inline unsigned int setCPosition(unsigned int new_col) { col_position = new_col; redraw(); return col_position; }
 		
-		inline unsigned int setRSize(unsigned int new_row_size);
-		inline unsigned int setCSize(unsigned int new_col_size);
+		inline unsigned int setRSize(unsigned int new_row_size) { row_size = new_row_size; redraw(); return row_size; }
+		inline unsigned int setCSize(unsigned int new_col_size) { col_size = new_col_size; redraw(); return new_col_size; }
+		
+		inline void setTextColor(byte new_color) { text_color = new_color & 0x0f; }
+		inline void setBackgroundColor(byte new_color) { background_color = new_color & 0x0f; }
+		inline void setBorderColor(byte new_color) { border_color = new_color & 0x0f; }
+		
+		inline byte getTextColor(byte new_color) const { return text_color; }
+		inline byte getBackgroundColor(byte new_color) const { return background_color; }
+		inline byte getBorderColor(byte new_color) const { return border_color; }
 		
 		inline ETUIObject * GetParent() const;
 		inline void SetParent(ETUIObject * p_new_parent);
@@ -33,8 +43,17 @@ class ETUIObject
 		bool visible;
 		ETUIObject * p_parent;
 		
+		byte text_color;
+		byte background_color;
+		byte border_color;
+		char border_symbol;
+		
+		void printToBuffer(int x_pos, int y_pos, char out_char, byte format);
+		
 		// this is essentially for virtual functions.  
 		ETUIObject * base_pointer;
+		
+		static int screen_width;
 };
 
 #endif 

7826176ff8d9edda4c88db95a9899a46d5ffa7d7
Eric Hamilton <inarchus@gmail.com>
2024-07-05 16:19:54 -0400

    Can a C++ class method be implemented in assembly? Yes

---
+++ msfat.asm
@@ -0,0 +1,14 @@
+[bits 32]
+
+extern _ZN5msfat12FATPartition20ExperimentalFunctionEjj
+
+section .text
+
+_ZN5msfat12FATPartition20ExperimentalFunctionEjj:
+	push ebp
+	mov ebp, esp
+	mov eax, [ebp + 8]
+	mov ebx, [ebp + 12]
+	add eax, ebx
+	leave
+	ret
